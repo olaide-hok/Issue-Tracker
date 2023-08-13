5ce4496e3f85f626985f838304157b89659708ea
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-08-13 15:37:05 +0100

    Created custom useUserData hook

---
+++ src/helpers/useUserData.js
@@ -0,0 +1,8 @@
+import {useQuery} from 'react-query'
+
+export const useUserData = (userId) => {
+    const usersData = useQuery(['users', userId], () =>
+        fetch(`/api/users/${userId}`).then((res) => res.json())
+    )
+    return usersData
+}
