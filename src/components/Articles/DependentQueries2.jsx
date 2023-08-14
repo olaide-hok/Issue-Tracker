19579f218a6e732a00ea8b8d4fbdd741935a6f3c
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-08-14 22:06:10 +0100

    Dependent Queries using mulitple useQuery hook.

---
+++ src/components/Articles/DependentQueries2.jsx
@@ -0,0 +1,43 @@
+const IssueLabelFilter = ({owner, repo}) => {
+    const labelsQuery = useQuery(['repos', owner, repo, 'labels'], () =>
+        fetch(`https://api.github.com/repos/${owner}/${repo}/labels`).then(
+            (res) => res.json()
+        )
+    )
+
+    const labels = labelsQuery.data
+
+    const issuesQuery = useQuery(['repos', owner, repo, 'issues'], () =>
+        fetch(
+            `https://api.github.com/repos/${owner}/${repo}/issues?labels=${labels[0].name}`
+        ).then((res) => res.json())
+    )
+
+    return (
+        <div>
+            <h2>Labels</h2>
+            {labelsQuery.isLoading ? (
+                <p>Loading labels...</p>
+            ) : (
+                <ul>
+                    {labelsQuery.data.map((label) => (
+                        <li key={label.id}>{label.name}</li>
+                    ))}
+                </ul>
+            )}
+
+            <hr />
+
+            <h2>Issues</h2>
+            {issuesQuery.isLoading ? (
+                <p>Loading issues...</p>
+            ) : (
+                <ul>
+                    {issuesQuery.data.map((issue) => (
+                        <li key={issue.id}>{issue.title}</li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
+}
