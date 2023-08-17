d545987a28fb9591c7ebc11079c631246ad29355
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-08-17 19:59:59 +0100

    Refactor Issues page

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,17 +1,32 @@
-import IssuesList from "../components/IssuesList";
-import LabelList from "../components/LabelList";
+import IssuesList from '../components/IssuesList'
+import LabelList from '../components/LabelList'
+import {useState} from 'react'
+
 export default function Issues() {
-  return (
-    <div>
-      <main>
-        <section>
-          <h1>Issues</h1>
-          <IssuesList />
-        </section>
-        <aside>
-          <LabelList />
-        </aside>
-      </main>
-    </div>
-  );
+    const [labels, setLabels] = useState([])
+    return (
+        <div>
+            <main>
+                <section>
+                    <h1>Issues</h1>
+                    <IssuesList labels={labels} />
+                </section>
+                <aside>
+                    <LabelList
+                        selected={labels}
+                        toggle={(label) =>
+                            setLabels((currentLabels) =>
+                                currentLabels.includes(label)
+                                    ? currentLabels.filter(
+                                          (currentLabel) =>
+                                              currentLabel !== label
+                                      )
+                                    : currentLabels.concat(label)
+                            )
+                        }
+                    />
+                </aside>
+            </main>
+        </div>
+    )
 }
