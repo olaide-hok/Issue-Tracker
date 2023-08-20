dae2202c398a58ffd429c0ae78210bda142b45b9
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-08-20 15:01:41 +0100

    Create useLabelsData hook.
    This returns the labelsQuery data.

--- src/helpers/useLabelsData.js
+++ src/helpers/useLabelsData.js
@@ -2,7 +2,7 @@ import {useQuery} from 'react-query'
 
 export const useLabelsData = () => {
     const labelsQuery = useQuery(['labels'], () =>
-        fetch('/api/labels').then((res) => res.json)
+        fetch('/api/labels').then((res) => res.json())
     )
     return labelsQuery
 }
