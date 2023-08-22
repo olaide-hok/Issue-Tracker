473780bea11ce0d82f59d6606b62d18d74a73b36
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-08-22 18:34:40 +0100

    Update issuesQuery to use fetchWithError fn.
    Add headers obj of 'x-error': true.
    
    This forces the API response to be either success or failure.
    
    Conditionally render error response to the UI.

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import {useQuery} from 'react-query'
 import {IssueItem} from './IssueItem'
+import fetchWithError from '../helpers/fetchWithError'
 
 export default function IssuesList({labels, status}) {
     const issuesQuery = useQuery(
@@ -10,8 +11,13 @@ export default function IssuesList({labels, status}) {
             const labelsString = labels
                 .map((label) => `labels[]=${label}`)
                 .join('&')
-            return fetch(`/api/issues?${labelsString}${statusString}`).then(
-                (res) => res.json()
+            return fetchWithError(
+                `/api/issues?${labelsString}${statusString}`,
+                {
+                    headers: {
+                        'x-error': true,
+                    },
+                }
             )
         },
         {
@@ -54,6 +60,8 @@ export default function IssuesList({labels, status}) {
             <h2>Issues List</h2>
             {issuesQuery.isLoading ? (
                 <p>Loading...</p>
+            ) : issuesQuery.isError ? (
+                <p>{issuesQuery.error.message}</p>
             ) : searchQuery.fetchStatus === 'idle' &&
               searchQuery.isLoading === true ? (
                 <ul className="issues-list">
