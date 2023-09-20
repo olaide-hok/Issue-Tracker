6773eff25f0d020fa8137f633204845384e376ea
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-09-20 15:18:13 +0100

    Update IssueItem to use prefetchInfiniteQuery func
    This prefetches comments for infinite scroll options.

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -25,9 +25,10 @@ export const IssueItem = ({
                 queryClient.prefetchQuery(['issues', number.toString()], () =>
                     fetchWithError(`/api/issues/${number}`)
                 )
-                queryClient.prefetchQuery(
+                queryClient.prefetchInfiniteQuery(
                     ['issues', number.toString(), 'comments'],
-                    () => fetchWithError(`/api/issues/${number}/comments`)
+                    () =>
+                        fetchWithError(`/api/issues/${number}/comments?page=1`)
                 )
             }}>
             <div>
